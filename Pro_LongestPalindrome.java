632e2f214424b67897d319ce21fb64043681c33b
wl9322 <72476869+wl9322@users.noreply.github.com>
2022-04-07 10:25:54 +0900

    Add files via upload

---
+++ Pro_LongestPalindrome.java
@@ -0,0 +1,27 @@
+
+public class Pro_LongestPalindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "abcdcba";
+		solution(s);
+	}
+	
+    public static int solution(String s) {
+        int answer = 1, len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        for(int idx = 0; idx < len; ++idx) dp[idx][idx] = true;
+        
+        for(int l = 1; l < len; ++l) {
+        	for(int start = 0; start < len - l; ++start) {
+        		int end = start + l;
+        		if(s.charAt(start) == s.charAt(end)) {
+        			if(start + 1 > end - 1 || dp[start + 1][end - 1]) dp[start][end] = true;
+        		}
+        	}
+        }
+        
+        return answer;
+    }
+
+}
