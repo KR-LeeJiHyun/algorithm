3c411061ade09555fab8185c940f5a3fba09a21c
wl9322 <wl9322@gmail.com>
2023-06-05 11:17:02 +0900

    백준 이분탐색 문제

--- Baek공유기설치.java
+++ Baek공유기설치.java
@@ -13,41 +13,37 @@ public class Baek공유기설치 {
 	static int[] router;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		
-//		StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int idx = 0;
-		++idx;
-		System.out.println(idx);
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-//		N = Integer.parseInt(st.nextToken());
-//		C = Integer.parseInt(st.nextToken());
-//		router = new int[N];
-//		
-//		for(int idx = 0; idx < N; ++idx) {
-//			router[idx] = Integer.parseInt(br.readLine());
-//		}
-//		
-//		Arrays.sort(router);
-//		
-//		int left = 1;
-//		int right = router[N - 1] - router[0];
-//		
-//		while(left <= right) {
-//			int mid = (left + right) / 2;
-//			
-//			if(calc(mid) < C) {
-//				right = mid - 1;
-//			}else {
-//				left = mid + 1;
-//			}
-//		}
+		N = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		router = new int[N];
 		
-//		bw.write(String.valueOf(left - 1));
-//		br.close();
-//		bw.close();
+		for(int idx = 0; idx < N; ++idx) {
+			router[idx] = Integer.parseInt(br.readLine());
+		}
+		
+		Arrays.sort(router);
+		
+		int left = 1;
+		int right = router[N - 1] - router[0];
+		
+		while(left <= right) {
+			int mid = (left + right) / 2;
+			
+			if(calc(mid) < C) {
+				right = mid - 1;
+			}else {
+				left = mid + 1;
+			}
+		}
+		
+		bw.write(String.valueOf(left - 1));
+		br.close();
+		bw.close();
 
 	}
 
