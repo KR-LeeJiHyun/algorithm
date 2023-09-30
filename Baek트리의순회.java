7fc66e11be6cc4ab0493fc658034cde8e5e9c3b2
wl9322 <wl9322@gmail.com>
2023-09-30 22:10:45 +0900

    백준 분할정복 문제

--- Baek트리의순회.java
+++ Baek트리의순회.java
@@ -10,7 +10,7 @@ public class Baek트리의순회 {
 	static int N;
 	static int[] in;
 	static int[] post;
-	static boolean[] visited; 
+	static int[] index; 
 	static StringBuilder answer;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -20,50 +20,30 @@ public class Baek트리의순회 {
 		N = Integer.parseInt(br.readLine());
 		in = new int[N];
 		post = new int[N];
-		visited = new boolean[N + 1];
+		index = new int[N + 1];
 		StringTokenizer input = new StringTokenizer(br.readLine());
 		StringTokenizer input2 = new StringTokenizer(br.readLine());
 		for(int idx = 0; idx < N; ++idx) {
-			in[idx] = Integer.parseInt(input.nextToken());
+			index[in[idx] = Integer.parseInt(input.nextToken())] = idx;
 			post[idx] = Integer.parseInt(input2.nextToken());
 		}
 		answer = new StringBuilder();
-		makeTree(0, N - 1);
+		makeTree(0, N - 1, 0, N - 1);
 		
  		bw.write(answer.toString());
 		br.close();
 		bw.close();
 	}
 
-	private static void makeTree(int left, int right) {
-		if(left > right) return;
-		
-		int root = post[right];
+	private static void makeTree(int is, int ie, int ps, int pe) {
+		if(is > ie) return;
+		int root = post[pe];
 		answer.append(root);
 		answer.append(' ');
-		
-		if(left == right) return;
-		
-		Arrays.fill(visited, false);
-		int stand = -1;
-		for(int idx = 0; idx < N; ++idx) {
-			if(in[idx] == root) {
-				break;
-			}
-			visited[in[idx]] = true;
-		}
-		for(int idx = right - 1; idx >= 0; --idx) {
-			if(visited[post[idx]]) {
-				//왼쪽 서브트리의 루트
-				stand = idx;
-				break;
-			}
-		}
-		
-		//왼쪽
-		makeTree(left, stand);
-		//오른쪽
-		makeTree(stand + 1, right - 1);
+		if(is == ie) return;
+		int size = index[root] - is;
+		makeTree(is, is + size - 1, ps, ps + size - 1);
+		makeTree(index[root] + 1, ie, ps + size, pe - 1);
 	}
 
 }
