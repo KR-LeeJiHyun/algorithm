927a1a4944fcf0c8c55efce54f044fe7451b15f2
wl9322 <wl9322@gmail.com>
2024-02-21 17:29:16 +0900

    백준 공통조상 문제

--- Baek정점들의거리.java
+++ Baek정점들의거리.java
@@ -11,24 +11,19 @@ import java.util.StringTokenizer;
 
 public class Baek정점들의거리 {
 
-	static class Node implements Comparable<Node>{
+	static class Node {
 		int v;
 		int w;
-		int l;
 
-		public Node(int v, int w, int l) {
+		public Node(int v, int w) {
 			this.v = v;
 			this.w = w;
-			this.l = l;
-		}
-
-		public int compareTo(Node o) {
-			return this.w - o.w;
 		}
 	}
 	
-	static int[] parents;
+	static int[][] parents;
 	static int[] levels;
+	static int maxH;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -46,32 +41,36 @@ public class Baek정점들의거리 {
 			int u = Integer.parseInt(input.nextToken());
 			int v = Integer.parseInt(input.nextToken());
 			int w = Integer.parseInt(input.nextToken());
-			map[u].add(new Node(v , w, 0));
-			map[v].add(new Node(u , w, 0));
+			map[u].add(new Node(v , w));
+			map[v].add(new Node(u , w));
 		}
 
 		int M = Integer.parseInt(br.readLine());
 		StringBuilder answer = new StringBuilder();
 		int[] dist = new int[N + 1];
-		parents = new int[N + 1];
+		maxH = (int)Math.ceil(Math.log(N) / Math.log(2)) + 1;
+		parents = new int[N + 1][maxH];
 		levels = new int[N + 1];
 		boolean[] visited = new boolean[N + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[1] = 0;
-		parents[1] = 1;
 		visited[1] = true;
 		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(1, 0, 0));
+		q.add(new Node(1, 0));
 		while(!q.isEmpty()) {
 			Node c = q.poll();
 			for(Node next : map[c.v]) {
 				if(!visited[next.v]) {
 					visited[next.v] = true;
 					dist[next.v] = c.w + next.w;
-					parents[next.v] = c.v;
-					levels[next.v] = c.l + 1;
-					q.add(new Node(next.v, dist[next.v], levels[next.v]));
+					parents[next.v][0] = c.v;
+					levels[next.v] = levels[c.v] + 1;
+					q.add(new Node(next.v, dist[next.v]));
 				}
+			}
+		}
+		
+		for(int col = 1; col < maxH; ++col) {
+			for(int row = 1; row <= N; ++row) {
+				parents[row][col] = parents[parents[row][col - 1]][col - 1];
 			}
 		}
 		
@@ -91,10 +90,25 @@ public class Baek정점들의거리 {
 	}
 
 	private static int find(int start, int end) {
+		int sl = levels[start];
+		int el = levels[end];
+		
+		if(sl < el) return find(end, start);
+		
+		for(int idx = maxH - 1; idx >= 0; --idx) {
+			if(Math.pow(2, idx) <= levels[start] - el) start = parents[start][idx];
+		}
+		
 		if(start == end) return start;
-		else if(levels[start] < levels[end]) return find(start, parents[end]);
-		else if(levels[start] > levels[end]) return find(parents[start], end);
-		else return find(parents[start], parents[end]);
+		
+		for(int idx = maxH - 1; idx >= 0; --idx) {
+			if(parents[start][idx] != parents[end][idx]) {
+				start = parents[start][idx];
+				end = parents[end][idx];
+			}
+		}
+		
+		return parents[start][0];
 	}
 
 }
